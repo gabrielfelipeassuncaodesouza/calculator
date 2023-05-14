63c29c12183c0c8d67892483fb137be293f4c316
gabriel <gabrielfelipeassuncaodesouza@gmail.com>
2023-05-14 00:40:50 -0300

    refactor the script.js and redo the css code

--- src/script.js
+++ src/script.js
@@ -1,46 +1,45 @@
+//TODO: colocar bootstrap
+//Global variables
 var firstValue
 var operation
-
 const visor = document.getElementById("visor")
 
 function execute(value) {
-  
   visor.value += value.toString()
 }
 
 function chooseOperator(operator) {
-  
   firstValue = parseInt(visor.value)
   visor.value = ""
   operation = operator
 }
 
-function limpar() {
-    
+function limpar() { 
     visor.value = ""
     firstValue = 0
     operation = ""
 }
 
-function returnValue() {
-  
-  switch(operation) {
-      
-      case '+': 
-      visor.value = firstValue + parseInt(visor.value)
-      break
-      
-      case '-': 
-      visor.value = firstValue - parseInt(visor.value)
-      break
-      
-      case 'x':
-      visor.value = firstValue * parseInt(visor.value)
-      break
-      
-      case '÷': 
-      visor.value = firstValue /parseInt(visor.value)
-      break
-      
+const doOperation = {
+  plus: function(value1, value2) {
+    visor.value = value1 + value2
+  },
+    
+  minus: function(value1, value2) {
+    visor.value = value1 - value2
+  },
+    
+  times: function(value1, value2) {
+    visor.value = value1 * value2
+  },
+    
+  divided: function(value1, value2) {
+    visor.value = value1 / value2
   }
+}
+
+function calcular() {
+  const auxFunc = doOperation[operation]
+  let secondValue= parseInt(visor.value)
+  auxFunc(firstValue, secondValue)
 }
